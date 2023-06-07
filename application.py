50b984076a034e4e68192cf8b9e29ea094f3a38e
jeremy alexander <jeremyalexander60@gmail.com>
2023-06-07 12:08:13 +1000

    add dev branch

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
 
     html.Div(children='''
-        This is Dash running on Azure App Service.
+        This is a Dash App running on a dev branch deployed using Docker and Azure App Service.
     '''),
 
     dcc.Graph(
